f19a733740f7f4c2a04df78a1f4c9a2be95abb35
JS3322 <js@cleancode.kr>
2022-04-24 17:53:41 +0900

    20220424 add sample curriculum

---
+++ pages/lectureOutline.js
@@ -0,0 +1,24 @@
+import {
+  Container,
+  ListItem,
+  UnorderedList
+} from '@chakra-ui/react'
+import { WorkImage } from '../../components/work'
+
+const lectureOutline = () => {
+  return (
+    <Container>
+      <UnorderedList my={4}>
+        <WorkImage src="/images/5tudy/sampleCurriculumOverview.png" alt="sampleCurriculumOverview" />
+   
+        <ListItem>
+          C++, Python, Java, Javascript, Go 언어와 코딩에 필요한 지식 개요도 
+        </ListItem>
+
+      </UnorderedList>
+ 
+    </Container>
+  )
+}
+
+export default lectureOutline
