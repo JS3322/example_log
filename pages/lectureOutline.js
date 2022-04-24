e2a61c8de7043b5d4193bdfa5f555cddf0b9a3b3
JS3322 <js@cleancode.kr>
2022-04-24 23:10:37 +0900

    20220424 test view

--- pages/lectureOutline.js
+++ pages/lectureOutline.js
@@ -15,6 +15,24 @@ const lectureOutline = () => {
           C++, Python, Java, Javascript, Go 언어와 코딩에 필요한 지식 개요도 
         </ListItem>
 
+        <WorkImage src="/images/5tudy/sampleCurriculumOverview.png" alt="sampleCurriculumOverview" />
+   
+        <ListItem>
+          C++ 커리큘럼 중 다형성 
+        </ListItem>
+
+        <WorkImage src="/images/5tudy/sampleCurriculumOverview.png" alt="sampleCurriculumOverview" />
+   
+        <ListItem>
+          Java 커리큘럼 중 리플렉션 설명
+        </ListItem>
+
+        <WorkImage src="/images/5tudy/sampleCurriculumOverview.png" alt="sampleCurriculumOverview" />
+   
+        <ListItem>
+          Python 커리큘럼 중 
+        </ListItem>
+
       </UnorderedList>
  
     </Container>
