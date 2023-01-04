7636384d8bab0a4bb161cebedd46c0934303c69d
js3322 <js@cleancode.kr>
2023-01-05 06:14:22 +0900

    youtube link

---
+++ pages/youtube.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from "react";
+import {Container, Divider, Heading, Text} from "@chakra-ui/react";
+
+function Background() {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            // 👇️ redirects to an external URL
+            window.location.replace('https://www.youtube.com/channel/UCWMhQtVmjXSyCKJ1SG5VjOA');
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    return (
+        <Container>
+            <Heading as="h2" variant="page-title">
+                Will redirect in 3 seconds...
+            </Heading>
+            <Divider my={6} />
+        </Container>
+    );
+}
+
+const lectureOutline = () => {
+  return (
+      <><Background/></>
+  )
+}
+
+export default lectureOutline
