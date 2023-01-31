00195718846c7a6b698835dbe6fffd01c335bebb
js3322 <js@cleancode.kr>
2023-01-31 11:29:15 +0900

    fix 2023 project

--- pages/works/2022_Worket.js
+++ pages/works/2022_Worket.js
@@ -18,7 +18,7 @@ const Work = () => (
   <Layout title="2022 Worket">
     <Container>
       <Title>
-      [진행] 데이터 저장, 분석 시스템 구현 <Badge>2022</Badge>
+      [진행] 모니터링 데이터기반 분석 시스템 Open Source <Badge>2023</Badge>
       </Title>
 
       <Heading as="h4" fontSize={16} my={6}>
@@ -27,18 +27,18 @@ const Work = () => (
 
       <UnorderedList my={4}>
         <ListItem>
-        수집 된 데이터 분석과 시각화디자인하고 데이터 관리시스템 구현
+        수집 된 데이터 분석 기반 관리시스템 구현
         </ListItem>
       </UnorderedList>
 
       <List ml={4} my={4}>
         <ListItem>
           <Badge mr={2}>Stack</Badge>
-          <span>Go1.19 | JavascriptEC6 | python3.9 | Django2.2 | CSS3 | Next.js</span>
+          <span>Go1.19 | JavascriptEC6 | python3.9 | Springboot3.0 | CSS3 | Next.js</span>
         </ListItem>
         <ListItem>
           <Meta>ENV</Meta>
-          <span>MSSQL | RedisDB | GKE | OCI | Docker | REST API | Nginx | JWT | Illustrator | Blender | MagicVoxel | Ploty | Pandas</span>
+          <span>Xpand | MariaDB | OCI | Docker-Swarm | REST API | Traefik | Illustrator | Pandas | Kafka</span>
         </ListItem>
       </List>
 
@@ -48,7 +48,7 @@ const Work = () => (
         ml={{ md: 6 }}
         textAlign="center"
       >
-      <NextLink href="https://github.com/JS3322/project_worket">
+      <NextLink href="https://github.com/JS3322/monitoring_in_action.git">
         <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
           Source Link
         </Button>
@@ -62,15 +62,11 @@ const Work = () => (
       <UnorderedList my={4}>
 
         <ListItem>
-          데이터 수집 및 모델링 데이터 API 배포 시스템 구축. 워크로드에 자동 파이프라인 세팅으로 Cloud Build API 사용하여 CI/CD 구축.
+          데이터 수집 및 분석 API 시스템 구축. 네이티브 자동 파이프라인 세팅으로 Cloud Build API 사용하여 CI/CD 구축.
         </ListItem>
 
         <ListItem>
-          데이터 통계 시스템 구현. Python dash 라이브러리 시각그래프 출력. 오버샘플링과 가중치 조절로 부족한 데이터 전처리 병행.
-        </ListItem>
-
-        <ListItem>
-          문서 작성과 관리. Notion을 활용한 API, 아키텍트, 개발환경 메뉴얼 작성. 마인드맵을 사용하여 통계 알고리즘과 브레인스토밍.
+          데이터 통계 모델링 구축. Log데이터 기반으로 Docker-Swarm 및 DB서버 샤딩 로드밸런싱 구축.
         </ListItem>
 
       </UnorderedList>
