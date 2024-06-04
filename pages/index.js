94597057d232a4aba5c24c93a9fd6ef04279d4a4
js3322 <js@cleancode.kr>
2024-06-04 21:12:19 +0900

    fix bioSection

--- pages/index.js
+++ pages/index.js
@@ -79,19 +79,19 @@ const Home = () => (
         </Heading>
           <BioSection>
             <BioYear>2018</BioYear>
-              GRIT 개발팀장 :: 데이터통계
+              GRIT 개발팀장 :: 데이터 엔지니어
           </BioSection>
           <BioSection>
             <BioYear>2020</BioYear>
-              옵티마이즈컨설팅 대표이사 :: 데이터 기반 컨설팅
+              옵티마이즈컨설팅 대표이사 :: 데이터 컨설팅
           </BioSection>
           <BioSection>
             <BioYear>2022</BioYear>
-              프리랜서 :: 백엔드 & 데이터 엔지니어
+              프리랜서 :: 데이터 사이언티스트
           </BioSection>
           <BioSection>
               <BioYear>2024</BioYear>
-              클린코드 대표 :: 데이터 사이언티스트
+              클린코드 대표 :: 데이터 엔지니어
           </BioSection>
       </Section>
 
