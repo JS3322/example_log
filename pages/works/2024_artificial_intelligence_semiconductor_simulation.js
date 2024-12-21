b36f1297d4367492b8c20f2aede0dbaa2b760bf9
js3322 <js@cleancode.kr>
2024-12-21 17:26:54 +0900

    update 20241221

--- pages/works/2024_artificial_intelligence_semiconductor_simulation.js
+++ pages/works/2024_artificial_intelligence_semiconductor_simulation.js
@@ -76,13 +76,22 @@ const Work = () => (
 
       <UnorderedList my={4}>
         <ListItem>
-          인공지능 시뮬레이션 솔루션 개발 기획 및 설계
+          fast api 기반 반도체 deck 분석 및 HPC 연산 상태정보 조회 서비스 구축
         </ListItem>
         <ListItem>
-          인공지능 모델 관리 설계 및 MLOps 시스템 기획 및 구축
+          airflow 및 greenplum 기반 queue 서버 구성 후 mlops기능 다중 노드 서버 분산작업 개발
         </ListItem>
         <ListItem>
-          HPC 내 인공지능 학습 모니터링 시스템 구축
+          wfmap deck 데이터 기반으로 학습에 필요한 데이터 생성(DOE) 시스템 설계
+        </ListItem>
+        <ListItem>
+          tensorflow 기반 반도체 공정 예측 인공지능 모델 프로토타입 개발 (전처리, 스케일링, 학습, 테스트, 추론, 최적화 단위별 모듈)
+        </ListItem>
+        <ListItem>
+          fast api와 lay 기반으로 prediction/ optimizer serving 시스템 구축
+        </ListItem>
+        <ListItem>
+          free treading 및 lay로 pytorch 기반 logic/ dram 반도체 공정 prediction/ optimizer 기능 코드 재설계 및 고도화
         </ListItem>
 
       </UnorderedList>
